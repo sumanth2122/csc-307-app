58861409115d5f203910c5c2efa70ee8c2b6d7ae
Sumanth <tsumanthr@gmail.com>
2024-10-01 16:47:36 -0700

    step 8

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Table from "./Table"
+import Form from "./Form"
 
 const characters = [
     {
@@ -52,6 +53,7 @@ function MyApp() {
     <div className="container">
         <Table characterData={characters}
         removeCharacter={removeOneCharacter} />
+        <Form />
     </div>
     )
 }
