b3e7bca2661d98bb79f8d135b4aef71da4932042
Sumanth <tsumanthr@gmail.com>
2024-10-11 15:11:41 -0700

    get users by id

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -39,6 +39,9 @@ const findUserByName = (name) => {
     );
 };
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -55,6 +58,16 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; 
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 
 
 app.listen(port, () => {
